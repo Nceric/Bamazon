1f2a051d5451e9d5b3fc0910d8ea7b1dbed36547
Nermin Ceric <nerceric@gmail.com>
2019-09-18 22:32:31 -0500

    added code to be able to buy products

--- BamazonSchema.sql
+++ BamazonSchema.sql
@@ -5,7 +5,7 @@ CREATE database bamazon_db;
 USE bamazon_db;
 
 CREATE TABLE products (
-id INTEGER(10) AUTO INCREMENT NOT NULL,
+id INTEGER(11) AUTO INCREMENT NOT NULL,
 product_name VARCHAR(30) NULL,
 department_name VARCHAR(30) NULL,
 price INT(10) NULL,
@@ -13,14 +13,14 @@ stock_quantity INT(10) NULL,
 PRIMARY KEY (id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Chicken', 'Meats', '5', '10');
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Beef', 'Meats', '5', '10');
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Turkey', 'Meats', '5', '10');
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Pork', 'Meats', '3', '10');
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Cheddar', 'Cheese', '3', '5');
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Provolone', 'Cheese', '3', '5');
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Swiss', 'Cheese', '4', '5');
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Havarti', 'Cheese', '4', '5');
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('American', 'Cheese', '3', '5');
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Feta', 'Cheese', '5', '5');
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Brie', 'Cheese', '5', '5');
+INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('1', 'Chicken', 'Meats', '5', '10');
+INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('2', 'Beef', 'Meats', '5', '10');
+INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('3', 'Turkey', 'Meats', '5', '10');
+INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('4', 'Pork', 'Meats', '3', '10');
+INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('5', 'Cheddar', 'Cheese', '3', '5');
+INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('6', 'Provolone', 'Cheese', '3', '5');
+INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('7', 'Swiss', 'Cheese', '4', '5');
+INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('8', 'Havarti', 'Cheese', '4', '5');
+INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('9', 'American', 'Cheese', '3', '5');
+INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('10', 'Feta', 'Cheese', '5', '5');
+INSERT INTO products (id, product_name, department_name, price, stock_quantity) values ('11', 'Brie', 'Cheese', '5', '5');
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,55 +13,67 @@ var connection = mysql.createConnection({
 
   connection.connect(function(err) {
     if (err) throw err;
-        showProducts();
+        buyProducts();
       });
-  
-      function showProducts() {
-        inquirer.prompt ([
-          {
-            type: "input",
-            message: "Enter item ID:",
-            name: "itemID"
-          }
-
-        ])
-      }
 
 
    Htable = new Table({
-    head: ['Product', 'Department', 'Price', 'Stock Quantity'],
-   colWidths: [10, 20,10,20]
+    head: ['id', 'Product', 'Department', 'Price', 'Stock Quantity'],
+   colWidths: [10, 10, 20,10,20]
 });
 
 Htable.push(
-  ['Chicken', 'Meat', '5', '10'],
-  ['Beef', 'Meat', '5', '10'],
-  ['Turkey', 'Meat', '5', '10'],
-  ['Pork', 'Meat', '3', '10'],
-  ['Cheddar', 'Cheese', '3', '5'],
-  ['Provolone', 'Cheese', '3', '5'],
-  ['Swiss', 'Cheese', '4', '5'],
-  ['Havarti', 'Cheese', '4', '5'],
-  ['American', 'Cheese', '3', '5'],
-  ['Feta', 'Cheese', '5', '5'],
-  ['Brie', 'Cheese', '5', '5'],
+  ['1', 'Chicken', 'Meat', '5', '10'],
+  ['2', 'Beef', 'Meat', '5', '10'],
+  ['3', 'Turkey', 'Meat', '5', '10'],
+  ['4', 'Pork', 'Meat', '3', '10'],
+  ['5', 'Cheddar', 'Cheese', '3', '5'],
+  ['6', 'Provolone', 'Cheese', '3', '5'],
+  ['7', 'Swiss', 'Cheese', '4', '5'],
+  ['8', 'Havarti', 'Cheese', '4', '5'],
+  ['9', 'American', 'Cheese', '3', '5'],
+  ['10', 'Feta', 'Cheese', '5', '5'],
+  ['11', 'Brie', 'Cheese', '5', '5'],
  
 );
 
 console.log(Htable.toString());
 
-
-
+function buyProducts() {
   inquirer
-  .prompt([
-    { type: "input", message: "Enter item ID", name: "itemID"}
+  .prompt([ 
+    { type: "input", message: "Enter item ID you wish to buy:", name: "itemID", filter: Number},
+    { type: "input", message: "Enter the amount to buy:", name: "quantityID", filter: Number},
+  
 
   ])
-
-  .then(answers => {
-      console.log(answers.itemID);
-
+  .then(function (answers) {
+    const productID = answers.itemID;
+    const quantityWanted = answers.quantityID;
+    salesTicket(productID, quantityWanted);
   });
+  
+
+function salesTicket(itemID, combinedAmnt) {
+  connection.query('Select * FROM products WHERE id = ' + itemID, function(err,res){
+    console.log(err, res)
+    if (err) throw err
+    if (combinedAmnt <= res[0].stock_quantity){
+      const totalPrice = res[0].price * combinedAmnt;
+      console.log("The total cost for " + combinedAmnt + " " +res[0].product_name + " is " + totalPrice + ", Thanks Come Again!");
+      connection.query("UPDATE products SET stock_quantity = stock_quantity - " + combinedAmnt + "WHERE id = " + itemID);
+    }
+  else {
+    console.log("Insufficient Quantity!" +res[0].product_name);
+  };
+  
+
+});
+buyProducts();
+}};
+
+
+
   
 
 
